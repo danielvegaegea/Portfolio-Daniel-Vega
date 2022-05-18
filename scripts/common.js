3f03061c9878dd78ed1643b0132a1a49802b4f2f
Daniel Vega Egea <d.vega.egea@gmail.com>
2022-05-18 17:51:39 +0200

    Improving responsivity V5.

--- scripts/common.js
+++ scripts/common.js
@@ -1,25 +1,59 @@
 // Element
 const burgerBtn = document.getElementsByClassName('btn-menu')[0];
+const burgerBTNStyle = getComputedStyle(burgerBtn);
 const sideBar = document.getElementsByClassName('sidebar')[0];
 const mainMenu = document.getElementsByTagName('nav')[0];
 let burgerMenu = false;
 
-// Event
-burgerBtn.addEventListener('click', showMenu);
-
-// Execution
-function showMenu(event) {
+// Function
+const showMenu = function (event) {
   if (!burgerMenu) {
-    sideBar.style.width = '99vw';
+    sideBar.classList.remove('hidden');
+    //sideBar.style.width = '99vw';
     mainMenu.style.display = 'flex';
-    mainMenu.style.width = '99vw';
+    //mainMenu.style.width = '99vw';
     //mainMenu.style.transition = '6s ease width';
     burgerBtn.textContent = 'X';
     burgerMenu = true;
   } else {
-    sideBar.style.width = '3.3rem';
-    mainMenu.style.display = 'none';
+    sideBar.classList.add('hidden');
+    //sideBar.style.width = '3.3rem';
+    //mainMenu.style.display = 'none';
     burgerBtn.textContent = '☰';
     burgerMenu = false;
   }
+  /* Old method
+  if (!burgerMenu) {
+    //sideBar.style.width = '99vw';
+    mainMenu.style.display = 'flex';
+    //mainMenu.style.width = '99vw';
+    //mainMenu.style.transition = '6s ease width';
+    burgerBtn.textContent = 'X';
+    burgerMenu = true;
+  } else {
+    //sideBar.style.width = '3.3rem';
+    //mainMenu.style.display = 'none';
+    burgerBtn.textContent = '☰';
+    burgerMenu = false;
+  }
+  */
+};
+
+const noMobile = function () {
+  if (window.innerWidth > 480) {
+    sideBar.classList.remove('hidden');
+  } else {
+    console.log(`bla`);
+    sideBar.classList.add('hidden');
+  }
+};
+
+// Execution
+if (window.innerWidth <= 480) {
+  //Alternativa: (burgerBTNStyle.visibility == 'visible')
+  sideBar.classList.add('hidden');
 }
+
+// Event
+burgerBtn.addEventListener('click', showMenu);
+window.addEventListener('resize', noMobile);
