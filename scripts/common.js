b2688e49e99457fb7c17c700c658d21d5f57093e
Daniel Vega Egea <d.vega.egea@gmail.com>
2022-05-23 16:07:00 +0200

    Adding image zooming.

--- scripts/common.js
+++ scripts/common.js
@@ -1,15 +1,28 @@
 // Element
-// Mobile Burger Menu
+
+//    Mobile Burger Menu
 const burgerBtn = document.getElementsByClassName('btn-menu')[0];
 const sideBar = document.getElementsByClassName('sidebar')[0];
 let burgerMenuSt = false;
-// Font Button
+
+//    Font Button
 const fontBtn = document.getElementsByClassName('btn-font')[0];
 let bigFontsSt = false;
 const sideBtns = document.getElementsByClassName('navButton');
 const footer = document.getElementsByTagName('footer')[0];
 const main = document.getElementsByTagName('main')[0];
-// Disabled but maintained as alternative.
+
+//    Modal
+let modalTrigger = document.getElementsByClassName('zoom');
+//let modalTrigger = document.getElementsByClassName('zoom')[0];
+const modalContainer = document.getElementsByClassName('modal-container')[0];
+const imageModalContainer = document.getElementsByClassName(
+  'modal-image-container',
+)[0];
+const modalClose = document.getElementsByClassName('close-btn')[0];
+let modal = document.querySelectorAll('.modal')[0];
+
+//    Disabled but maintained as alternative.
 //const burgerBtnStyle = getComputedStyle(burgerBtn);
 //getElementsByClassName('navButton');
 //const mainMenu = document.getElementsByTagName('nav')[0];
@@ -63,6 +76,30 @@ const noMobile = function () {
   }
 };
 
+const openModal = function (path) {
+  console.log(`a`);
+  console.log(path);
+  const imageZoomed = document.createElement('img');
+  imageZoomed.classList.add('image');
+  imageZoomed.src = path;
+  imageModalContainer.appendChild(imageZoomed);
+  console.log('c');
+  modalContainer.classList.remove('modal-close');
+  modalContainer.classList.add('modal-open');
+  //wrong
+  //modalContainer.classList.toggle('modal-close');
+};
+
+const closeModal = function () {
+  console.log(`b`);
+  const deleteImage = imageModalContainer.firstChild;
+  imageModalContainer.removeChild(deleteImage);
+  modalContainer.classList.remove('modal-open');
+  modalContainer.classList.add('modal-close');
+  //wrong
+  //modalContainer.classList.toggle('modal-close');
+};
+
 // Execution
 if (window.innerWidth <= 480) {
   // Executed once to hide menu after loading the web.
@@ -74,6 +111,16 @@ if (window.innerWidth <= 480) {
 burgerBtn.addEventListener('click', showMenu);
 fontBtn.addEventListener('click', bigFonts);
 window.addEventListener('resize', noMobile);
+Array.from(modalTrigger).forEach((index) => {
+  //console.log(index);
+  index.addEventListener('click', function (e) {
+    let zoomingImage = index.firstElementChild.src;
+    openModal(zoomingImage);
+  });
+  //modalTrigger[index.addEventListener('click', openModal);
+});
+modalClose.addEventListener('click', closeModal);
+modalContainer.addEventListener('click', closeModal);
 
 //bigFontsBtn
 //bigFonts
